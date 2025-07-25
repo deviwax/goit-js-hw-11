ff3e71f59c285a4ebae3cb90c0513cafd60b616e
deviwax <156660735+deviwax@users.noreply.github.com>
2025-07-26 00:29:31 +0300

    modified

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,5 +1,6 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const galleryContainer = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
